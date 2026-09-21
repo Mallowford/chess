278eb41496bfeb9d6c42e16d4097c456df0397ab
mallowford <mallowford@gmail.com>
2026-09-20 19:21:25 -0700

    Adding basic structure

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
-bool debounce();
+enum Piece_Type {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
+};
+
+struct chessPiece {
+    sf::Sprite sprite;
+    bool White = true;
+    Piece_Type piece = Pawn;
+};
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "My Window");
@@ -14,14 +28,20 @@ int main() {
 
     sf::Texture texture("../assets/chess_pieces/pawn.png");
     sf::Sprite sprite(texture);
-
+    sprite.setPosition({0.0f, 0.0f});
     sprite.scale({0.02f, 0.02f});
-
+    std::vector<sf::Sprite> board;
+    std::vector<chessPiece> pieces;
 
     std::cout << sprite.getScale().x << "\n";
     std::cout << sprite.getScale().y << "\n";
 
-    sf::RectangleShape board({120.0f, 50.0f});
+    sf::RectangleShape tile1({800.0f, 600.0f});
+    tile1.setFillColor(sf::Color(118, 150, 86));
+
+    sf::RectangleShape tile2({400.0f, 600.0f});
+    tile2.setFillColor(sf::Color(238, 238, 210));
+    tile2.move({400, 0});
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -38,8 +58,9 @@ int main() {
 
         window.clear(sf::Color::Black);
 
+        window.draw(tile1);
+        window.draw(tile2);
         window.draw(sprite);
-        window.draw(board);
 
         window.display();        
     };
